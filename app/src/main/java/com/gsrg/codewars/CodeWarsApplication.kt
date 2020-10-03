0b9f682bd414845ff46ace691c789b0f6dcbe13d
Gabriel Guedes <>
2020-10-03 12:09:32 +0100

    Added Timber and defined as a Hilt application

---
+++ app/src/main/java/com/gsrg/codewars/CodeWarsApplication.kt
@@ -0,0 +1,18 @@
+package com.gsrg.codewars
+
+import android.app.Application
+import com.facebook.stetho.Stetho
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+open class CodeWarsApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Stetho.initializeWithDefaults(this)
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
