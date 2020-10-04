d8e1b38493669d3cbebfac2bde8b2049ad41d185
Gabriel Guedes <>
2020-10-04 17:05:36 +0100

    PlayersDao.kt created and tested with PlayersDaoTest.kt

---
+++ database/src/androidTest/java/com/gsrg/codewars/database/players/PlayersDaoTest.kt
@@ -0,0 +1,108 @@
+package com.gsrg.codewars.database.players
+
+import android.content.Context
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.matcher.ViewMatchers.assertThat
+import com.gsrg.codewars.database.CodeWarsDatabase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import java.io.IOException
+
+@ExperimentalCoroutinesApi
+class PlayersDaoTest {
+
+    private val testDispatcher = TestCoroutineDispatcher()
+
+    private lateinit var playersDao: PlayersDao
+    private lateinit var codeWarsDatabase: CodeWarsDatabase
+
+    @Before
+    fun createDb() {
+        Dispatchers.setMain(testDispatcher)
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        codeWarsDatabase = Room.inMemoryDatabaseBuilder(
+            context, CodeWarsDatabase::class.java
+        ).build()
+        playersDao = codeWarsDatabase.playersDao()
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun deleteDataAndCloseDatabase() {
+        codeWarsDatabase.close()
+        Dispatchers.resetMain()
+        testDispatcher.cleanupTestCoroutines()
+    }
+
+    @Test
+    fun insertPlayerInDatabase() = runBlocking {
+        val player = Player("AAA", 5)
+        playersDao.insert(player)
+        val playersList: List<Player> = playersDao.selectAllPlayers()!!
+        assertThat(playersList[0], equalTo(player))
+    }
+
+    @Test
+    fun insertMultiplePlayersAndKeep5() = runBlocking {
+        playersDao.insert(Player("AAA", 1))
+        playersDao.keepLast5Players()
+        var playerList: List<Player> = playersDao.selectAllPlayers()!!
+        Assert.assertEquals(1, playerList.size)
+
+        playersDao.insert(Player("AAB", 2))
+        playersDao.keepLast5Players()
+        playerList = playersDao.selectAllPlayers()!!
+        Assert.assertEquals(2, playerList.size)
+
+        playersDao.insert(Player("AAC", 3))
+        playersDao.keepLast5Players()
+        playerList = playersDao.selectAllPlayers()!!
+        Assert.assertEquals(3, playerList.size)
+
+        playersDao.insert(Player("AAD", 4))
+        playersDao.keepLast5Players()
+        playerList = playersDao.selectAllPlayers()!!
+        Assert.assertEquals(4, playerList.size)
+
+        playersDao.insert(Player("AAE", 5))
+        playersDao.keepLast5Players()
+        playerList = playersDao.selectAllPlayers()!!
+        Assert.assertEquals(5, playerList.size)
+
+        playersDao.insert(Player("AAF", 6))
+        playersDao.keepLast5Players()
+        playerList = playersDao.selectAllPlayers()!!
+        Assert.assertEquals(5, playerList.size)
+
+        playersDao.insert(Player("AAG", 7))
+        playersDao.keepLast5Players()
+        playerList = playersDao.selectAllPlayers()!!
+        Assert.assertEquals(5, playerList.size)
+
+        playersDao.insert(Player("AAH", 8))
+        playersDao.keepLast5Players()
+        playerList = playersDao.selectAllPlayers()!!
+        Assert.assertEquals(5, playerList.size)
+
+        playersDao.insert(Player("AAI", 9))
+        playersDao.keepLast5Players()
+        playerList = playersDao.selectAllPlayers()!!
+        Assert.assertEquals(5, playerList.size)
+
+        playersDao.insert(Player("AAJ", 10))
+        playersDao.keepLast5Players()
+        playerList = playersDao.selectAllPlayers()!!
+        Assert.assertEquals(5, playerList.size)
+
+    }
+}
---
+++ database/src/main/java/com/gsrg/codewars/database/CodeWarsDatabase.kt
@@ -0,0 +1,36 @@
+package com.gsrg.codewars.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.gsrg.codewars.database.players.Player
+import com.gsrg.codewars.database.players.PlayersDao
+
+@Database(
+    entities = [Player::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class CodeWarsDatabase : RoomDatabase() {
+
+    abstract fun playersDao(): PlayersDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: CodeWarsDatabase? = null
+
+        fun getInstance(context: Context): CodeWarsDatabase =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: buildDatabase(context).also { INSTANCE = it }
+            }
+
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                CodeWarsDatabase::class.java,
+                "CodeWars.db"
+            ).build()
+    }
+}
